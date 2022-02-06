3721f6c775b9b03a070e58ba8cf18ba5541604a9
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-02-06 19:57:06 +0600

    Start project and add to Firebase

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDR-5ivWLNoYfLqbWseuYuoJAkrs3jOJ3w",
+    authDomain: "ema-john-simple-a6f42.firebaseapp.com",
+    projectId: "ema-john-simple-a6f42",
+    storageBucket: "ema-john-simple-a6f42.appspot.com",
+    messagingSenderId: "274783054989",
+    appId: "1:274783054989:web:7ed3e711858a50323986c4"
+};
+
+export default firebaseConfig
