c538b043bbf259314fac58894b556c710221b0a6
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-02-07 00:11:55 +0600

    Setup firebase sign in, sign out and observer hook

---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,48 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
+
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+    const [error, setError] = useState('');
+
+    const googleProvider = new GoogleAuthProvider();
+
+    const auth = getAuth();
+
+    const signInUsingGoogle = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                console.log(result.user);
+                setUser(result.user);
+            })
+            .catch(error => {
+                setError(error.message, error.code);
+            })
+    }
+
+    const logout = () => {
+        signOut(auth)
+            .then(() => {
+                setUser({})
+            })
+            .catch((error) => {
+                setError(error.message)
+            });
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            }
+        })
+    }, [])
+
+    return {
+        user,
+        error,
+        signInUsingGoogle,
+        logout
+    }
+}
+export default useFirebase;
